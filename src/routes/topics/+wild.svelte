4671794d73f945476d87e4de9d7d79b572229181
blz <nate@blz.ai>
2023-01-08 16:33:07 -0800

    Setting up repo

---
+++ src/routes/topics/+wild.svelte
@@ -0,0 +1,12 @@
+<script>
+    // logic goes here
+</script>
+
+<!-- markup (zero or more items) goes here -->
+<div class="photos">
+    [#each photos as photo]
+    <figure></figure>
+</div>
+<style>
+    /* styles go here */
+</style>
