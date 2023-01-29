38dc669c3579e6f2cdb97c19ee9f8d7a67d0433d
BlazeFace <nate.bensing@gmail.com>
2023-01-28 17:58:33 -0800

    Update +layout.js

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,9 +1,6 @@
 // Documentation: https://kit.svelte.dev/docs/page-options#prerender
 export const prerender = true;
 
-// Documentation: https://kit.svelte.dev/docs/page-options#ssr
-export const ssr = true;
-
 // https://kit.svelte.dev/docs/page-options#trailingslash
-export const trailingSlash = 'ignore';
+export const trailingSlash = 'always';
 
