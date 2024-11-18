a576520abf97873b8a5f5bb2c1a369f46026d170
BlazeFace <nate.bensing@gmail.com>
2024-11-17 18:24:13 -0800

    Fix issues with label

--- src/routes/topics/+page.svelte
+++ src/routes/topics/+page.svelte
@@ -3,7 +3,6 @@
     import {parse} from 'yaml';
     import type {IStateWrapper} from "$lib/ITopics";
 
-    // export const prerender = true;
     const base: IStateWrapper = parse(PUBLIC_STATE);
     const topics = base.topics;
 </script>
