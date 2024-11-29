ce7fb68d72ef32ae069b4bd85ad0c08a4a470c4a
blz <contact@blz.ai>
2024-11-29 17:29:48 -0500

    Use System Theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'media',
   content: ['./src/**/*.{html,js,svelte,ts}'],
   daisyui: {
-    themes: ['fantasy', 'dracula']
+    themes: ['fantasy', 'dracula'],
+    darkTheme: 'dracula'
   },
   plugins: [require("@tailwindcss/typography"), require('daisyui')],
 };
