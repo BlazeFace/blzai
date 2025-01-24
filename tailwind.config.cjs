01c0b7da0728dce222ccef0f76a2c71d78cf017d
blz <contact@blz.ai>
2025-01-23 19:05:47 -0800

    Update Tailwind

--- tailwind.config.cjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'media',
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  plugins: [require("@tailwindcss/typography")],
-};
